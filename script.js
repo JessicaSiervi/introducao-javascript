96c5dd7e50757ef2cc3693a82de4fba7dd78ecdd
JessicaSiervi <jessicasiervi10@gmail.com>
2025-12-10 22:15:31 -0300

    att

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const sectionLogo = document.getElementsByClassName('logo')[1].innerHTML = "TESTE";
+const tituloDaHome = document.getElementById("title-home")
 
-console.log("section", sectionLogo)
+tituloDaHome.innerHTML = "Jéssica"
+tituloDaHome.style.color = "#c2c2c2"
+tituloDaHome.setAttribute("style", "color #101010 ; font-size:80px")
+
+const butoonHome = document.getElementById("btn-home")
+
+buttonHome.innerHTML = "Clique aqui"
+buttonHome.addEventListener('click', () => 
+    { tituloDaHome.innerHTML = "Jéssica Siervi" })
+
+const secondTitleId =document.getElementById ("second-title")
+const secondTitleQuery = document.querySelector ("#second-title")
+console.log('secondTitleQuery', secondTitleQuery)
+console.log('')
